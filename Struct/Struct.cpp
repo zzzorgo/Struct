0ab73fc4336a8737d8e993444eb7010f1911e23c
Dmitriy Selyanin <zzzorgo@yandex-team.ru>
2018-08-22 18:04:19 +0300

    store letters in vector

--- Struct/Struct.cpp
+++ Struct/Struct.cpp
@@ -2,11 +2,57 @@
 //
 
 #include "Struct.h"
+#include <vector>
 
 using namespace std;
 
+class LetterProbability {
+	public: 
+	char letter;
+	double probability;
+	int letterCount;
+
+	LetterProbability() {
+		probability = 0;
+		letterCount = 0;
+	}
+};
+
+ostream &operator<<(std::ostream &os, LetterProbability const &lp) { 
+    return os << lp.letter << " " << lp.letterCount << endl;
+}
+
 int main()
 {
-	cout << "Hello CMake." << endl;
+	vector<char> text;
+	char c;
+	while (cin >> c) {
+		text.push_back(c);
+	}
+
+	vector<LetterProbability> letterProbability;
+
+	for (int i = 0; i < text.capacity(); i++) {
+		char c = text[i];
+		auto foundedChar = find_if(letterProbability.begin(), letterProbability.end(), [&c](const LetterProbability& obj) {return obj.letter == c;});
+
+		int index = -1;
+		if (foundedChar != letterProbability.end()) {
+			index = distance(letterProbability.begin(), foundedChar);
+		}
+		if (index < 0) {
+			LetterProbability lp = LetterProbability();
+			lp.letter = text[index];
+			lp.letterCount++;
+			letterProbability.push_back(lp);
+		} else {
+			letterProbability[index].letterCount++;
+		}
+	}
+
+	for (auto lp : letterProbability) {
+		cout << lp << endl;
+	}
+
 	return 0;
 }
