66634bb3876b565101ab585d2dc6da4c97946369
Dmitriy Selyanin <zzzorgo@gmail.com>
2018-08-25 13:46:53 +0300

    temp

--- Struct/Struct.cpp
+++ Struct/Struct.cpp
@@ -3,6 +3,10 @@
 
 #include "Struct.h"
 #include <vector>
+#include <algorithm>
+#include <fstream> 
+#include <iterator>
+#include <conio.h>
 
 using namespace std;
 
@@ -19,22 +23,48 @@ class LetterProbability {
 };
 
 ostream &operator<<(std::ostream &os, LetterProbability const &lp) { 
-    return os << lp.letter << " " << lp.letterCount << endl;
+    return os << lp.letter << " " << lp.letterCount << " " << lp.probability << endl;
 }
+
+
+class TreeNode {
+public:
+	int priority;
+	LetterProbability initialNode;
+	TreeNode *leftNode;
+	TreeNode *rightNode;
+
+	TreeNode(LetterProbability initialNodeL) {
+		initialNode = initialNodeL;
+		priority = initialNode.letterCount;
+		leftNode = NULL;
+		rightNode = NULL;
+	}
+
+	TreeNode() {
+		priority = 0;
+	}
+};
 
 int main()
 {
-	vector<char> text;
-	char c;
-	while (cin >> c) {
-		text.push_back(c);
+	ifstream charFile("haffman.txt");
+	if (!charFile) {
+		return -1;
 	}
+	istream_iterator<char> start(charFile), end;
+	vector<char> text(start, end);
+    copy(text.begin(), text.end(),
+		ostream_iterator<char>(cout));
+	cout << endl;
+
 
 	vector<LetterProbability> letterProbability;
 
-	for (int i = 0; i < text.capacity(); i++) {
+	for (int i = 0; i < text.size(); i++) {
 		char c = text[i];
 		auto foundedChar = find_if(letterProbability.begin(), letterProbability.end(), [&c](const LetterProbability& obj) {return obj.letter == c;});
+		
 
 		int index = -1;
 		if (foundedChar != letterProbability.end()) {
@@ -42,7 +72,7 @@ int main()
 		}
 		if (index < 0) {
 			LetterProbability lp = LetterProbability();
-			lp.letter = text[index];
+			lp.letter = c;
 			lp.letterCount++;
 			letterProbability.push_back(lp);
 		} else {
@@ -50,9 +80,44 @@ int main()
 		}
 	}
 
+
+	sort(letterProbability.begin(), letterProbability.end(), [](const LetterProbability& a, const LetterProbability& b) {
+		return a.letterCount < b.letterCount;
+	});
+
 	for (auto lp : letterProbability) {
+		lp.probability = (double)lp.letterCount / text.size();
 		cout << lp << endl;
 	}
 
+	vector<TreeNode>treeVector;
+
+	for (LetterProbability lp : letterProbability) {
+		treeVector.push_back(TreeNode(lp));
+	}
+
+	for (int i = 0; i < letterProbability.size()-1; i++) {
+		TreeNode firstNode = treeVector[0];
+		TreeNode secondNode = treeVector[1];
+		treeVector.erase(treeVector.begin());
+		treeVector.erase(treeVector.begin());
+
+		TreeNode newNode = TreeNode();
+		newNode.priority = firstNode.priority + secondNode.priority;
+
+		auto p = &firstNode;
+
+		newNode.leftNode = p;
+		newNode.rightNode = &secondNode;
+
+		treeVector.push_back(newNode);
+		sort(treeVector.begin(), treeVector.end(), [](const TreeNode& a, const TreeNode& b) {
+			return a.priority < b.priority;
+		});
+		int a = 0;
+	}
+
+	
+	getch();
 	return 0;
 }
